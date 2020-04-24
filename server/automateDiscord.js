544fda26203350ee1847cd4afa213a1381af43ec
Jonathan Ming <jming422@gmail.com>
2020-04-24 15:04:13 -0700

    Automating Discord with puppeteer!

---
+++ server/automateDiscord.js
@@ -0,0 +1,54 @@
+const axios = require('axios').default;
+const lodash = require('lodash');
+const puppeteer = require('puppeteer-core');
+
+const DISCORD_DEBUG_PORT = process.env.DISCORD_DEBUG_PORT;
+
+const focusServerXPath = '//a[@aria-label="Focus Dev"]';
+
+const connectXPath = '//div[@role="button" and contains(@aria-label, "General (voice channel)")]';
+const disconnectXPath = '//button[@aria-label="Disconnect"]';
+
+async function getWSEndpoint() {
+  if (!DISCORD_DEBUG_PORT) {
+    throw Error(
+      `Environment variable DISCORD_DEBUG_PORT not found - you won't be able to connect to the Discord app without this!`
+    );
+  }
+  const resp = await axios.get(`http://localhost:${DISCORD_DEBUG_PORT}/json/list`);
+  return _(resp).get(['data', 0, 'websocketDebuggerurl']);
+}
+
+async function doInDiscord(fn) {
+  const browser = await puppeteer.connect({
+    browserWSEndpoint: await getWSEndpoint(),
+  });
+  const pages = await browser.pages();
+  const page = pages[0];
+  try {
+    await page.waitForXPath(focusServerXPath, { timeout: 9000 });
+    const [focusServerBtn] = await page.$x(focusServerXPath);
+    await focusServerBtn.click();
+    await fn(page);
+  } finally {
+    browser.disconnect();
+  }
+}
+
+async function sit() {
+  await doInDiscord(async (page) => {
+    await page.waitForXPath(connectXPath, { timeout: 3000 });
+    const [connectBtn] = page.$x(connectXPath);
+    await connectBtn.click();
+  });
+}
+
+async function stand() {
+  await doInDiscord(async (page) => {
+    await page.waitForXPath(disconnectXPath, { timeout: 3000 });
+    const [disconnectBtn] = page.$x(disconnectXPath);
+    await disconnectBtn.click();
+  });
+}
+
+module.exports = { sit, stand };
--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('@koa/router');
 
@@ -5,32 +7,30 @@ const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
 const respond = require('koa-respond');
 
-function sitFn() {
-  console.log('Sitting!');
-}
-
-function standFn() {
-  console.log('Standing!');
-}
+const { sit, stand } = require('./automateDiscord');
 
 const app = new Koa();
 const router = new Router();
 
 router.get('/', (ctx) => ctx.ok('Pong!'));
-router.post('/', (ctx) => {
+router.post('/', async (ctx) => {
   const body = ctx.request.body;
   console.log(`Got body: ${JSON.stringify(body)}`);
-
-  switch (body.sitting) {
-    case true:
-      sitFn();
-      break;
-    case false:
-      standFn();
-      break;
-    default:
-      console.warn('What?');
-      return ctx.badRequest();
+  try {
+    switch (body.sitting) {
+      case true:
+        await sit();
+        break;
+      case false:
+        await stand();
+        break;
+      default:
+        console.warn('What?');
+        return ctx.badRequest();
+    }
+  } catch (err) {
+    console.error(err);
+    return ctx.internalServerError();
   }
 
   return ctx.noContent();
@@ -42,5 +42,5 @@ app.use(respond());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const PORT = 12345;
+const PORT = process.env.SITCORD_PORT || 12345;
 const server = app.listen(PORT, () => console.log(`listening on port ${port}`));
